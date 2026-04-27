35a8a0d7f4282bda97923e04369b8d4509240e16
haniphiz <haniphiz132@gmail.com>
2026-04-27 17:04:59 +0700

    hapus node module

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const db = require("./config/db");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// endpoint test database
+app.get("/test-db", (req, res) => {
+  db.query("SELECT 1", (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error" });
+    }
+    res.json({
+      message: "Database connected",
+      result
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server jalan di http://localhost:${port}`);
+});
