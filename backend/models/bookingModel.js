03a4eddedb3077d0988e81d495d0747954f8e73f
Rian AF <rianakhlulfadli@gmail.com>
2026-04-30 22:48:59 +0700

    Delete backend directory

--- backend/models/bookingModel.js
+++
@@ -1,9 +0,0 @@
-const conn = require('../config/db');
-
-exports.getAllBookings = (callback) => {
-  conn.query("SELECT * FROM bookings", callback);
-};
-
-exports.addBooking = (data, callback) => {
-  conn.query("INSERT INTO bookings SET ?", data, callback);
-};
--- backend/routes/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getBookings, addBooking } = require('../controllers/bookingController');
-const router = express.Router();
-
-router.get('/', getBookings);
-router.post('/', addBooking);
-
-module.exports = router;
